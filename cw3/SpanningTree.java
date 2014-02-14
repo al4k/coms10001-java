b3de0af70d300c618e90b5d310fce192c728fea2
Anthony Bench <anthonybench@eduroam-255-105.nomadic.bris.ac.uk>
2014-02-14 17:31:43 +0000

    Updated SpanningTree.java

--- cw3/SpanningTree.java
+++ cw3/SpanningTree.java
@@ -4,8 +4,12 @@ import java.text.DateFormat;
 import java.text.DecimalFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 class SpanningTree {
 
@@ -20,13 +24,16 @@ class SpanningTree {
 			}
 			DecimalFormat twodp = new DecimalFormat("#.##");
 			if(args[0].equals("-p1")){
-				println("Total cable needed: "+twodp.format(totalEdgeWeight(r.graph()))+"m");
+				println("Total cable needed: "+twodp.format(totalCable(r.graph()))+"m");
 			}else if(args[0].equals("-p2")){
 				println("Price: £"+twodp.format(governmentCost(r.graph())));
 				println("Hours of disrupted travel: "+twodp.format(hourCost(r.graph()))+"h");
 				println("Completion date: "+dateComplete(r.graph()));
 			}else if(args[0].equals("-p3")){
-				
+				Graph g = computeSpanningTree(r.graph());
+				println("Price: £"+twodp.format(governmentCost(g)));
+				println("Hours of disrupted travel: "+twodp.format(hourCost(g))+"h");
+				println("Completion date: "+dateComplete(g));
 			}
 		}
 	}
@@ -35,7 +42,7 @@ class SpanningTree {
 		double total = 0;
 		for(Edge e : g.edges()){
 			switch(e.type()){
-				case LocalRoad : {total += 5000 + (e.weight() * 4000);}
+				case LocalRoad : {total += 5000 + (e.weight() * 4500);}
 				case MainRoad : {total += e.weight() * 4000;}
 				case Underground : {total += e.weight() * 1000;}
 			}
@@ -71,12 +78,66 @@ class SpanningTree {
 		return new SimpleDateFormat("EEEE, d, MMMM, YYYY HH:mm").format(new Date(current));
 	}
 	
-	private double totalEdgeWeight(Graph g){
+	private double totalCable(Graph g){
 		double total = 0;
 		for(Edge e : g.edges()){
-			total += e.weight();
+			total += e.weight() * 1000;
 		}
 		return total;
+	}
+	
+	private List<Edge> quickSort(List<Edge> edges){
+		if(!edges.isEmpty()){
+			Edge pivot = edges.get(0);
+			List<Edge> less = new ArrayList<Edge>();
+			List<Edge> pivotList = new ArrayList<Edge>();
+			List<Edge> more = new ArrayList<Edge>();
+
+			for(Edge e : edges){
+				if(e.weight() < pivot.weight()){
+					less.add(e);
+				}else if(e.weight() > pivot.weight()){
+					more.add(e);
+				}else{
+					pivotList.add(e);
+				}
+			}
+			less = quickSort(less);
+			more = quickSort(more);
+			less.addAll(pivotList);
+			less.addAll(more);
+			return less;
+		}
+		return edges;
+	}
+	
+	private Graph computeSpanningTree(Graph g){
+		List<Edge> edges = quickSort(g.edges());
+		HashMap<String, List<String>> forest = new HashMap<String, List<String>>();
+		Graph output = new Graph();
+		for(Node n : g.nodes()){
+			List<String> tree = new ArrayList<String>();
+			tree.add(n.name());
+			forest.put(n.name(), tree);
+			output.add(n);
+		}
+		
+		while(!edges.isEmpty()){
+			Edge e = edges.remove(0);
+			List<String> connects1 = forest.get(e.id1());
+			List<String> connects2 = forest.get(e.id2());
+			if(connects1.equals(connects2))
+				continue;
+			
+			output.add(e);
+			connects1.addAll(connects2);
+			for(String n : connects1){
+				forest.put(n, connects1);
+			}
+			if(connects1.size() == g.nodes().size())
+				break;	
+		}
+		return output;
 	}
 	
 	private static void println(Object s){
