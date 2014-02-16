cbc9d1396bbc98e850fbdf238df8724805eb7395
Anthony Bench <bench@mbp.local>
2014-02-16 15:48:17 +0000

    Fixed switch conditions

--- cw3/SpanningTree.java
+++ cw3/SpanningTree.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.text.*;
 import java.util.*;
 
@@ -16,25 +17,25 @@ class SpanningTree {
 			if(args[0].equals("-p1")){
 				println("Total cable needed: "+twodp.format(totalCable(r.graph()))+"m");
 			}else if(args[0].equals("-p2")){
-				println("Price: £"+twodp.format(governmentCost(r.graph())));
+				println("Price: £"+twodp.format(cashCost(r.graph())));
 				println("Hours of disrupted travel: "+twodp.format(hourCost(r.graph()))+"h");
 				println("Completion date: "+dateComplete(r.graph()));
 			}else if(args[0].equals("-p3")){
-				Graph g = minSpanningTree(r.graph());
-				println("Price: £"+twodp.format(governmentCost(g)));
-				println("Hours of disrupted travel: "+twodp.format(hourCost(g))+"h");
-				println("Completion date: "+dateComplete(g));
+				Graph min = minSpanningTree(r.graph());
+				println("Price: £"+twodp.format(cashCost(min)));
+				println("Hours of disrupted travel: "+twodp.format(hourCost(min))+"h");
+				println("Completion date: "+dateComplete(min));
 			}
 		}
 	}
 
-	private double governmentCost(Graph g){
+	private double cashCost(Graph g){
 		double total = 0;
-		for(Edge e : g.edges()){
+		for(Edge e : g.edges()){			
 			switch(e.type()){
-				case LocalRoad : {total += 5000 + (e.weight() * 4500);}
-				case MainRoad : {total += e.weight() * 4000;}
-				case Underground : {total += e.weight() * 1000;}
+				case LocalRoad : {total += (5000 + (e.weight() * 4500)); break;}
+				case MainRoad : {total += e.weight() * 4000; break;}
+				case Underground : {total += e.weight() * 1000; break;}
 			}
 		}
 		return total;
@@ -44,28 +45,27 @@ class SpanningTree {
 		double total = 0;
 		for(Edge e : g.edges()){
 			switch(e.type()){
-				case LocalRoad : {total += e.weight() * 0.2;}
-				case MainRoad : {total += e.weight() * 0.5;}
-				case Underground : {total += e.weight();}
+				case LocalRoad : {total += e.weight() * 0.2; break;}
+				case MainRoad : {total += e.weight() * 0.5; break;}
+				case Underground : {total += e.weight(); break;}
 			}
 		}
 		return total;
 	}
 	
-	private static long millisInDay = 86400000;
-	
-	private String dateComplete(Graph g){		
+	private String dateComplete(Graph g){	
+		final long millisInDay = 86400000;
 		Calendar c = Calendar.getInstance();
 		c.set(2014, 1, 15, 00, 00);
 		long current = c.getTimeInMillis();
 		for(Edge e : g.edges()){ 
 			switch(e.type()){
-				case LocalRoad : {current += (e.weight()/0.2)*millisInDay;}
-				case MainRoad : {current += (e.weight() / 0.6)*millisInDay;}
-				case Underground : {current += (e.weight() / 0.9)*millisInDay;}
+				case LocalRoad : {current += (e.weight()/0.2)*millisInDay; break;}
+				case MainRoad : {current += (e.weight() / 0.6)*millisInDay; break;}
+				case Underground : {current += (e.weight() / 0.9)*millisInDay; break;}
 			}
 		}
-		return new SimpleDateFormat("EEEE, d, MMMM, YYYY HH:mm").format(new Date(current));
+		return new SimpleDateFormat("EEEE d MMMM YYYY HH:mm").format(new Date(current));
 	}
 	
 	private double totalCable(Graph g){
@@ -121,18 +121,19 @@ class SpanningTree {
 		*/
 		while(!edges.isEmpty()){
 			Edge e = edges.remove(0);
-			List<String> nodes1 = forest.get(e.id1());
-			List<String> nodes2 = forest.get(e.id2());
-			if(nodes1.equals(nodes2))
+			List<String> tree1 = forest.get(e.id1());
+			List<String> tree2 = forest.get(e.id2());
+			
+			if(tree1.equals(tree2))
 				continue;
 			
 			output.add(e);
-			nodes1.addAll(nodes2);
+			tree1.addAll(tree2);
 			
-			for(String n : nodes1){
-				forest.put(n, nodes1);
+			for(String n : tree1){
+				forest.put(n, tree1);
 			}
-			if(nodes1.size() == g.nodes().size())
+			if(tree1.size() == g.nodes().size())
 				break;	
 		}
 		
